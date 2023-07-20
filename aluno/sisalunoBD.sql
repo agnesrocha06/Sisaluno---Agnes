e5514d8b76e6c7392c23362207233b73bfa1996f
agnesrocha06 <112827092+agnesrocha06@users.noreply.github.com>
2023-07-20 17:01:49 -0300

    Add files via upload
    Código do BD utilizado no sistema.

---
+++ aluno/sisalunoBD.sql
@@ -0,0 +1,22 @@
+DROP DATABASE sisaluno;
+
+CREATE DATABASE sisaluno;
+
+USE sisaluno;
+
+CREATE TABLE aluno(
+id VARCHAR(40) PRIMARY KEY,
+nome VARCHAR(60) NOT NULL,
+idade INT, 
+endereco VARCHAR(100) NOT NULL,
+curso VARCHAR(40) NOT NULL, 
+telefone INT, 
+turma CHAR(4)
+);
+
+SHOW TABLES;
+DESC aluno;
+SELECT * FROM aluno;
+
+ALTER TABLE aluno MODIFY id INT;
+
